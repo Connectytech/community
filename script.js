a11118ce0d26525096680da2a3e02a58b58a6f3f
Bruhar <andriambolanirina123@gmail.com>
2025-02-14 19:23:53 +0300

    fit: Mise en place de l'organigramme connecty tech

--- script.js
+++ script.js
@@ -1,45 +1,65 @@
-const links = document.querySelectorAll('a');
-const sections = document.querySelectorAll('section');
-let isScrolling = false;
+const links = document.querySelectorAll('a')
+const sections = document.querySelectorAll('section')
+let currentSection = null
 
-links.forEach((link) => {
-    link.addEventListener('click', () => {
-        changeState(link);
-        isScrolling = false;
-    });
-});
+const listActitiviesDescription = document.querySelectorAll('.activity-description')
+const listActitiviesItem = document.querySelectorAll('.items-activity')
+
+const listRoles = document.querySelectorAll('.role')
+const hiddenImage = document.querySelectorAll('.img-hidden')
 
 function changeState(item) {
     links.forEach((link) => {
         if(item === link){
-            link.classList.add('active');
+            link.classList.add('active')
         }else{
-            link.classList.remove('active');
+            link.classList.remove('active')
         }
-    });
+    })
 }
 
 function setActiveLink() {
-  if (!isScrolling) return;
-
-  let currentSection = null;
   
   sections.forEach(section => {
-    const rect = section.getBoundingClientRect();
+    const rect = section.getBoundingClientRect()
     if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
-      currentSection = section;
+      currentSection = section
     }
-  });
+  })
 
   if (currentSection) {
-    const activeLink = document.querySelector(`a[href="#${currentSection.id}"]`);
+    const activeLink = document.querySelector(`a[href="#${currentSection.id}"]`)
     changeState(activeLink)
   }
 }
 
-window.addEventListener('scroll', () => {
-    isScrolling = true;
-    setActiveLink();
-});
 
-setActiveLink();
+function addFonctionnality(parentList, chileList){
+  parentList.forEach((item, index) => {
+    item.addEventListener('mouseover', () => {
+      chileList[index].classList.add('visible')
+    })
+    
+    item.addEventListener('mouseleave', () => {
+      chileList.forEach((description) => {
+        description.classList.remove('visible')
+      })
+    })
+  })
+}
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+      changeState(link)
+  })
+})
+
+window.addEventListener('scroll', () => {
+    setActiveLink()
+    console.log(window)
+})
+
+setActiveLink()
+
+addFonctionnality(listActitiviesItem, listActitiviesDescription)
+addFonctionnality(listRoles, hiddenImage)
